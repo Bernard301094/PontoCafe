48afe9c81128bd689eefa0cbb5ead9651bcb5749
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-08-13 21:18:43 -0300

    Isola criacao do estado do aplicativo

---
+++ app/src/main/java/com/pontocafe/app/AppStateFactory.kt
@@ -0,0 +1,14 @@
+package com.pontocafe.app
+
+import com.pontocafe.app.camera.MissingFaceEmbeddingEngine
+import com.pontocafe.app.data.PontoCafeRepository
+import com.pontocafe.app.data.SecureDeviceTokenStore
+
+fun createPontoCafeViewModel(
+    repository: PontoCafeRepository,
+    tokenStore: SecureDeviceTokenStore,
+): PontoCafeViewModel = PontoCafeViewModel(
+    repository = repository,
+    tokenStore = tokenStore,
+    embeddingEngine = MissingFaceEmbeddingEngine,
+)
