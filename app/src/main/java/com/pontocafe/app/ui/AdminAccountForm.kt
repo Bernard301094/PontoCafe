2a30b359d35081f1b9c31caa6294cd7336d1459c
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-08-14 15:31:38 -0300

    Corrige compatibilidade do formulario admin com Material 3

--- app/src/main/java/com/pontocafe/app/ui/AdminAccountForm.kt
+++ app/src/main/java/com/pontocafe/app/ui/AdminAccountForm.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -33,6 +34,7 @@ data class NewAccountInput(
     val perfil: AccountProfile,
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AdminAccountForm(
     carregando: Boolean = false,
