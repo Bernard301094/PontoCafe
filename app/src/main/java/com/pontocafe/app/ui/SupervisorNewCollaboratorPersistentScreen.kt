d6aabaa149bd349df167177bfea1ea75991bade0
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-08-17 15:06:15 -0300

    fix(ui): adiciona import de tamanho no cadastro do supervisor

--- app/src/main/java/com/pontocafe/app/ui/SupervisorNewCollaboratorPersistentScreen.kt
+++ app/src/main/java/com/pontocafe/app/ui/SupervisorNewCollaboratorPersistentScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
