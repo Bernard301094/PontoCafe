c1ffb14d0714741dd7161c4cfbb46694b024d0c1
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-08-17 16:53:06 -0300

    chore(android): prepara versão 0.9.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "com.pontocafe.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "0.9.2"
+        versionCode = 21
+        versionName = "0.9.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
