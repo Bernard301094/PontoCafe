43af5dd5d6b8f25bf754efd4733e1dc61d421055
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-08-17 13:15:44 -0300

    Corrige import da central de sincronização

--- app/src/main/java/com/pontocafe/app/ui/SyncCenterScreen.kt
+++ app/src/main/java/com/pontocafe/app/ui/SyncCenterScreen.kt
@@ -1,7 +1,6 @@
 package com.pontocafe.app.ui
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.AlignmentLine
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
