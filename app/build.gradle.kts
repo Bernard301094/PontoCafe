d3e3fc8f1bfc59a759f232d4a249bea2b82694e8
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-08-17 11:58:04 -0300

    Prepara release visual 0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "com.pontocafe.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.7.1"
+        versionCode = 9
+        versionName = "0.8.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
