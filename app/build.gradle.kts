492e75839633ba05e93663bde5710e616dacf3ba
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-08-17 12:52:20 -0300

    Atualiza versão para 0.8.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "com.pontocafe.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.8.3"
+        versionCode = 13
+        versionName = "0.8.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
