1880dee717490ed17a4d51817b778e545e8d2a8b
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-08-17 13:15:12 -0300

    Corrige imports dos componentes UX

--- app/src/main/java/com/pontocafe/app/ui/OperationalUxComponents.kt
+++ app/src/main/java/com/pontocafe/app/ui/OperationalUxComponents.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
