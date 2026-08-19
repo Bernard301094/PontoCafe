95bd89600adf003c3552bc9330c5072f123db372
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-08-19 17:11:09 -0300

    chore: bump android release to 0.10.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "com.pontocafe.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.10.3"
+        versionCode = 26
+        versionName = "0.10.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
