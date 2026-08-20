5fb31e076673e9c15ea5c260ebc8545deb938cb4
Bernard De Freitas <114589760+Bernard301094@users.noreply.github.com>
2026-08-20 19:15:22 -0300

    Bump Android version to 0.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "com.pontocafe.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 34
-        versionName = "0.13.0"
+        versionCode = 35
+        versionName = "0.14.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
